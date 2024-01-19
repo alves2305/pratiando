542d9c6a7215ad1562e6d1ff038762b1f27dd90d
Rafael Alves <raffa.alvess@hotmail.com>
2024-01-19 18:07:56 -0300

    teste

---
+++ app.js
@@ -0,0 +1,11 @@
+function clicar(){
+    let conteudo = document.getElementById('conteudo')
+    conteudo.style.display = 'block'
+    let fundo = document.querySelector('body')
+    fundo.style.backgroundImage = 'url(Imagem\\ do\\ WhatsApp\\ de\\ 2024-01-19\\ à\\(s\\)\\ 18.00.52_602545d6.jpg)';
+    let botao = document.getElementById('CliqueEmMim')
+    botao.style.display = 'none'
+
+
+
+}
